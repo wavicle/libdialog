821eae5b84e920d85f808a6dda7cb940dfd154fb
Shashank Araokar <shashank.araokar@gmail.com>
2017-12-10 11:01:51 -0500

    Fixed documentation and examples

--- README.md
+++ README.md
@@ -2,35 +2,7 @@
 
 A Java library to quickly build command-line text-based applications. Features and usage are explained below.
 
+## Usage and examples
 
-## Read a simple line of text
-
-First, we need to import the DialogProcessor:
-
-```java
-import wavicle.libdialog.DialogProcessor;
-```
-
-Next, reading a line of text is as simple as:
-```java
-DialogProcessor processor = new DialogProcessor();
-String lineOfText = processor.input("Enter some line of text");
-System.out.println("You entered: " + lineOfText);
-```
-
-## Read a number (BigDecimal)
-
-Numbers are read as `BigDecimal`'s:
-
-```java
-BigDecimal number = processor.inputNumber("What is your favorite number? ");
-System.out.println("Your favorite number is: " + number);
-```
-
-The library provides basic validation on the input - if the number is not valid, 
-the following error is shown and the user is asked for the input again:
-
-```
-Not a valid number: '***' - please retry: 
-```
+Check the class `wavicle.libdialog.SysDemo` in the `test` folder for a detailed example.
 
--- wavicle.libdialog/src/test/java/wavicle/libdialog/SysDemo.java
+++ wavicle.libdialog/src/test/java/wavicle/libdialog/SysDemo.java
@@ -3,14 +3,11 @@
  */
 package wavicle.libdialog;
 
-import java.util.Arrays;
-
-import wavicle.libdialog.DialogProcessor;
-
-import static java.lang.System.out;
+import java.math.BigDecimal;
 
 /**
- * The Class SysDemo.
+ * Demonstrates how command-line applications are built using the {@link Sys}
+ * class in the libdialog library.
  */
 public class SysDemo {
 
@@ -21,11 +18,59 @@ public class SysDemo {
      *            the arguments
      */
     public static void main(String[] args) {
-        out.println("Your name is: " + Sys.input("What is your name (try an empty string)? ", DialogProcessor.NOT_EMPTY_STRING_VALIDATOR));
+        /** We will use these variables throughout this demo **/
+        String line;
+        BigDecimal number;
 
-        out.println("The number you entered is: " + Sys.inputNumber("Please enter a number (try 'abc'): "));
+        /** Let's input a string from the command line **/
+        line = Sys.input("Please enter any text (press return at the end): ");
+        System.out.println("You entered: " + line);
 
-        out.println("The season you selected is: "
-                + Sys.inputOption("What is your favorite season? ", Arrays.asList("Spring", "Fall", "Autumn", "Winter")));
+        /**
+         * Note that we did not add any validations. So try entering nothing this time.
+         * It still works
+         **/
+        line = Sys.input("Just hit enter this time (empty text): ");
+        System.out.println("You entered: " + line);
+
+        /**
+         * Let's add some validation. We won't allow an emptry string (0-length or just
+         * white spaces). This is done by passing an instance of the InputValidator
+         * interface. Below, we are passing a specific implementation of that interface.
+         **/
+        line = Sys.input("Enter an empty string and I will prompt you again: ", DialogProcessor.NOT_EMPTY_STRING_VALIDATOR);
+        System.out.println("You entered the following non-empty string: " + line);
+
+        /**
+         * What if you wanted to input a number? We can read a BigDecimal directly. The
+         * expression has to be a valid number else it won't work.
+         **/
+        number = Sys.inputNumber("Enter a number (if you enter an invalid string like '12.34abc' I won't accept it: ");
+        System.out.println("The valid number you entered is: " + number);
+
+        /**
+         * Like with plain strings, you can add validations to numbers too. Note that we
+         * are also overriding the buildErrorMessage method in this case to provide a
+         * custom validation error message.
+         * 
+         * In the validator below, we validate that the number is from 1 to 10.
+         **/
+        InputValidator<BigDecimal> numberValidator = new InputValidator<BigDecimal>() {
+
+            @Override
+            public boolean isValid(BigDecimal input) {
+                /** Greater than or equal to 1, less than or equal to 10 **/
+                return input.compareTo(BigDecimal.ONE) >= 0 && input.compareTo(BigDecimal.TEN) <= 0;
+            }
+
+            /** This method is overridden to provide a custom message **/
+            @Override
+            public String buildErrorMessage(BigDecimal input) {
+                return "The number " + input + " is not in the range 1 to 10! Please try again: ";
+            }
+        };
+        number = Sys.inputNumber("Please enter a number (try entering a negative number to see the validation): ", numberValidator);
+        System.out.println("The number you entered is: " + number);
+
     }
 }
